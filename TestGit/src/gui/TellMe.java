9852ee35c855f98130938ec6d930eabd13d60106
hellobramah <hellobramah@gmail.com>
2014-12-06 06:45:59 +0100

    Initial commit on the TellMe java file

---
+++ TestGit/src/gui/TellMe.java
@@ -0,0 +1,10 @@
+package gui;
+import javax.swing.*;
+public class TellMe {
+public static void main(String args[]){
+	JOptionPane.showMessageDialog(null, "Good Work Done, Alex, You are the best");
+	
+	
+	
+}
+}
