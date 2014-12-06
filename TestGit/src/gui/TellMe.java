19317eccaa913501f52f98ba885c015c7b0c86b1
hellobramah <hellobramah@gmail.com>
2014-12-06 11:55:17 +0100

    Change in Input

--- TestGit/src/gui/TellMe.java
+++ TestGit/src/gui/TellMe.java
@@ -2,7 +2,8 @@ package gui;
 import javax.swing.*;
 public class TellMe {
 public static void main(String args[]){
-	JOptionPane.showMessageDialog(null, "Good Work Done, Alex, You are the best");
+	String name=JOptionPane.showInputDialog(null, "Please enter your name");
+	JOptionPane.showMessageDialog(null, "Good Work Done, "+name+", You are the best");
 	
 	
 	
