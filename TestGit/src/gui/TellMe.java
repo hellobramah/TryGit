99720e99b6051b2077542602ce6e5066ad7e72a3
hellobramah <hellobramah@gmail.com>
2014-12-09 09:34:33 +0100

    Something added

--- TestGit/src/gui/TellMe.java
+++ TestGit/src/gui/TellMe.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class TellMe {
 public static void main(String args[]){
 	String name=JOptionPane.showInputDialog(null, "Please enter your name");
-	JOptionPane.showMessageDialog(null, "Good Work Done, "+name+", You are the best");
+	JOptionPane.showMessageDialog(null, "Good Work Done, God bless you mightily "+name+", You are the best");
 	
 	
 	
